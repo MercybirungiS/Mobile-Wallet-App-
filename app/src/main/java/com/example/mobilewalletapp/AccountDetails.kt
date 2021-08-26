4dc99763a28313d83828ae09ea2fac03cc63282d
MercybirungiS <mercyssbirungi@gmail.com>
2021-08-26 17:50:33 +0300

    adaptor

---
+++ app/src/main/java/com/example/mobilewalletapp/AccountDetails.kt
@@ -0,0 +1,9 @@
+package com.example.mobilewalletapp
+
+data class AccountDetails(
+    var description:String,
+    var transaction:String,
+    var date:String,
+    var amount:Int,
+    var transactionType:String
+)
